fd1b4afc7b7b737fee1baa47154fedbed4ebf161
gustavocotalima <gustavolima@unifei.edu.br>
2023-06-03 00:40:03 -0300

    Created users route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
-import http from 'node:http';
+import http from 'node:http'
 
 const server = http.createServer((request, response) => {
-    return response.end('Hello World!');
+    const { method, url } = request;
+
+    if (method === 'GET' && url === '/users')
+        return response.end('Listagem de Usuários')
+    if (method === 'POST' && url === '/users')
+        return response.end('Criação de Usuário')
+
+    return response.end('Hello World!')
 });
 
 server.listen(3333);
