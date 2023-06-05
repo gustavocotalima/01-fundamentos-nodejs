9eaf58b8876969222dd3fd18a3e4eaa0730f597e
gustavocotalima <gustavocotalima@hotmail.com>
2023-06-05 11:21:15 -0300

    atualização de registrods

--- src/database.js
+++ src/database.js
@@ -38,6 +38,14 @@ export class Database {
         return data
     }
 
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table][rowIndex] = { id, ...data }
+        }
+    }
+
     delete(table, id) {
         const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
@@ -46,5 +54,6 @@ export class Database {
             this.#persist()
         }
     }
+    
 
 }
